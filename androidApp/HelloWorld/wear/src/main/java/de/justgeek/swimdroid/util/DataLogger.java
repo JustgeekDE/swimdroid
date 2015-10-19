5a15fae852295bd26a40846d7291dc6dd60ed6fb
Philip Peter <github@justgeek.de>
2015-10-19 11:13:26 +0200

    Fixing data format for logs

--- androidApp/HelloWorld/wear/src/main/java/de/justgeek/swimdroid/util/DataLogger.java
+++ androidApp/HelloWorld/wear/src/main/java/de/justgeek/swimdroid/util/DataLogger.java
@@ -7,7 +7,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Locale;
 
 /**
  * Created by ppeter on 06/10/2015.
@@ -46,11 +49,28 @@ public class DataLogger {
 
     public void storeSensorEvent(SensorEvent event, long timeOffset) {
         long timestamp = (event.timestamp - timeOffset) / 1000000l;
-        String dataString = timestamp + ", " + Arrays.toString(event.values).replace("[", "").replace("]", "") + "\n";
+        String dataString = timestamp + ", " + sensorEventToString(event) + "\n";
         try {
             outputStream.write(dataString.getBytes());
         } catch (IOException e) {
         }
+    }
+
+    private String sensorEventToString(SensorEvent event) {
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getNumberInstance(Locale.ENGLISH);
+        formatter.applyPattern("###.##");
+
+        StringBuilder builder = new StringBuilder();
+        boolean first = true;
+        for (Float data: event.values) {
+            if (!first) {
+                builder.append(",");
+            }
+            builder.append(formatter.format(data));
+            first = false;
+        }
+
+        return builder.toString();
     }
 
     public void store(String data) {
