9cf65711690a5674285b9bec7766aa3cb11c87ca
Philip Peter <github@justgeek.de>
2015-10-19 13:27:11 +0200

    Better breaktime estimation

--- androidApp/HelloWorld/wear/src/main/java/de/justgeek/swimdroid/processing/detectors/BreakDetector.java
+++ androidApp/HelloWorld/wear/src/main/java/de/justgeek/swimdroid/processing/detectors/BreakDetector.java
@@ -1,24 +1,36 @@
 package de.justgeek.swimdroid.processing.detectors;
 
 
+import de.justgeek.swimdroid.processing.Average;
+import de.justgeek.swimdroid.util.DataLogger;
+
 public class BreakDetector {
 
-    public static final int MAX_STROKE_TIME = 5000;
+    public static final float MAX_STROKE_VARIANCE = 2.0f;
     long lastStroke;
     long accumulatedBreakTime;
+    Average strokeTimeAverage = new Average(0, 0.95f, 10);
+    DataLogger logger = new DataLogger("breaks");
 
     public void update(long timeStamp) {
+
         if (lastStroke == 0) {
             lastStroke = timeStamp;
             return;
         }
 
+
         long breakTime = timeStamp - lastStroke;
-        breakTime -= MAX_STROKE_TIME;
+        strokeTimeAverage.update(breakTime);
+
+        breakTime -= MAX_STROKE_VARIANCE * strokeTimeAverage.getAverage();
 
         if (breakTime > 0) {
             accumulatedBreakTime += breakTime;
         }
+        String data = String.format("%d, %f, %d, %d, %d\n", timeStamp, strokeTimeAverage.getAverage(), breakTime, accumulatedBreakTime, (breakTime > 0 ? 50: -50));
+        logger.store(data);
+
         lastStroke = timeStamp;
     }
 
